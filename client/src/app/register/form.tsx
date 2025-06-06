ac9b1aa169c04ba3eaea520144bb0121480f2d5c
rafa <rmxvgit@gmail.com>
2025-06-06 15:35:37 -0300

    push

--- client/src/app/register/form.tsx
+++ client/src/app/register/form.tsx
@@ -20,38 +20,88 @@ export default function RegisterForm() {
   }
 
   return (
-    <Formik
-      initialValues={{ name: "", password: "", bio: "", job: "" }}
-      onSubmit={formSubmit}
-    >
-      <Form>
-        <label htmlFor="name">nome:</label>
-        <Field type="text" id="name" name="name" required />
+    <div className="flex min-h-screen justify-center content-center">
+      <Formik
+        initialValues={{ name: "", password: "", bio: "", job: "student" }}
+        onSubmit={formSubmit}
+      >
+        <Form className="bg-blue-300 p-8 rounded shadow-lg flex flex-col gap gap-5 h-fit">
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-sm">
+              nome:
+            </label>
+            <Field
+              type="text"
+              id="name"
+              name="name"
+              className="bg-white border rounded px-2 py-1"
+              required
+            />
+          </div>
 
-        <label htmlFor="password">senha:</label>
-        <Field type="password" id="password" name="password" required />
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-sm">
+              senha:
+            </label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              className="bg-white border rounded px-2 py-1"
+              required
+            />
+          </div>
 
-        <label htmlFor="bio">bio:</label>
-        <Field type="text" id="bio" name="bio" required />
+          <div className="flex flex-col">
+            <label htmlFor="bio" className="text-sm">
+              bio:
+            </label>
+            <Field
+              type="text"
+              id="bio"
+              name="bio"
+              className="bg-white border rounded px-2 py-1"
+              required
+            />
+          </div>
 
-        <label htmlFor="job">cargo:</label>
-        <Field as="select" name="job" id="job" required>
-          <option value="student">Estudante</option>
-          <option value="professor">Professor</option>
-        </Field>
+          <div className="flex flex-col">
+            <label htmlFor="job" className="text-sm">
+              cargo:
+            </label>
+            <Field
+              as="select"
+              name="job"
+              id="job"
+              className="bg-white border rounded px-2 py-1"
+              required
+            >
+              <option value="student">Estudante</option>
+              <option value="professor">Professor</option>
+            </Field>
+          </div>
 
-        <label htmlFor="image">image:</label>
-        <input
-          type="file"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setFile(
-              event.currentTarget.files ? event.currentTarget.files[0] : null,
-            );
-          }}
-        ></input>
-
-        <button type="submit">Register</button>
-      </Form>
-    </Formik>
+          <div className="flex flex-col">
+            <label htmlFor="image" className="text-sm">
+              image:
+            </label>
+            <input
+              type="file"
+              className="bg-white border rounded px-2 py-1"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setFile(
+                  event.currentTarget.files
+                    ? event.currentTarget.files[0]
+                    : null,
+                );
+              }}
+            ></input>
+          </div>
+          <button type="submit" className="bg-white border rounded px-2 py-1">
+            Register
+          </button>
+        </Form>
+      </Formik>
+    </div>
   );
 }
